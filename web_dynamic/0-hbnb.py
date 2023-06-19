83624e167c986decf03597e68c6c7358d2b13c30
kiki3165 <kikikou82@gmail.com>
2023-06-19 14:53:45 +0200

    fix some issues

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -22,7 +22,6 @@ def close_db(error):
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    cache_id = str(uuid.uuid4())
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -36,12 +35,13 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=cache_id)
+                           places=places,
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,23 +1,16 @@
-#!/usr/bin/node
-
-// Wait for the DOM to be loaded
-$(document).ready(function () {
-    // Create an empty object to store the checked amenities
-    let checkedAmenities = {};
-    // Listen for changes on each input checkbox tag
-    $('input[type="checkbox"]').change(function () {
-      // Get the data-id and data-name attributes of the checkbox
-      let amenityId = $(this).attr('data-id');
-      let amenityName = $(this).attr('data-name');
-      // If the checkbox is checked, add the amenity to the object
-      if ($(this).is(':checked')) {
-        checkedAmenities[amenityId] = amenityName;
-      } else {
-        // If the checkbox is unchecked, delete the amenity from the object
-        delete checkedAmenities[amenityId];
-      }
-      // Update the h4 tag inside the div Amenities with the list of amenities checked
-      let amenitiesList = Object.values(checkedAmenities).join(', ');
-      $('.amenities h4').text(amenitiesList);
-    });
+$('document').ready(function () {
+  const amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    let amenityNames = Object.values(amenities).join(', ');
+    // small list of amenties check finish by ...
+    if (amenityNames.length > 35) {
+      amenityNames = amenityNames.substring(0, 35) + '...';
+    }
+    $('.amenities h4').text(amenityNames);
   });
+});
